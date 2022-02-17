691c9a7281ac2ad4892fa7eb8aac97466807a07a
root <2604677382@qq.com>
2022-02-17 18:01:24 +0800

    项目的初始化

---
+++ src/HelloWork.java
@@ -0,0 +1,13 @@
+public class HelloWork {
+
+    /*public static void main(String[] args) {
+        System.out.println("Hello Work");
+        //System.out.println("h");
+    }*/
+
+    public static void main(String[] args){
+        System.out.println("p");
+        System.out.println("Hello Work");
+        System.out.println("Hello Work");
+    }
+}
